23dc4084dcf14601913eb64659049f057529222e
Andrés Nuñez <andresanunez98@gmail.com>
2021-04-12 14:22:04 -0400

    created 1rst migration

--- db/migrate/20210412180053_create_radio_stations.rb
+++ db/migrate/20210412180053_create_radio_stations.rb
@@ -1,8 +1,11 @@
 class CreateRadioStations < ActiveRecord::Migration[5.2]
   def change
     create_table :radio_stations do | t |
+      t.name :string
       t.string :username
       t.string :password_digest
+      t.region :string
+      
     end
   end
 end
