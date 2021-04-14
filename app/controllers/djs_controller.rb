5622259c202b0a74bbb635e8b2690d9da91870a3
Andrés Nuñez <andresanunez98@gmail.com>
2021-04-14 15:02:00 -0400

    added dj edit form

--- app/controllers/djs_controller.rb
+++ app/controllers/djs_controller.rb
@@ -33,7 +33,25 @@ get '/djs/:id' do
     @dj = Dj.find_by(id: params[:id] )
     @playlists = Dj.find_by(id: params[:id]).playlists
     erb :'djs/show'
+    
 
 end
+
+
+get '/djs/:id/edit' do
+    if logged_in?
+        @dj = Dj.find_by(id: params[:id] )
+        if @dj.id != current_dj.id || @dj.id == nil
+            redirect '/playlists'
+         else
+            erb :'djs/edit'
+        end  
+    else
+        redirect '/login'
+    end
+
+end
+
+
 
 end
